05b14acacf6b7bc3edd778d5ae960154e766228e
Jackson-Matthew <164759934+Jackson-Matthew@users.noreply.github.com>
2025-04-19 18:14:58 +0200

    Added Response functions
    The child class ResponseSystem Inherits from BotInterface and uses the dictionary as well as typingeffect methods

--- ChatBot_V1.0/ChatBot_V1.0/ResponseSystem.cs
+++ ChatBot_V1.0/ChatBot_V1.0/ResponseSystem.cs
@@ -2,8 +2,35 @@
 {
     class ResponseSystem : BotInterface
     {
+        public void Response()
+        {
+            TypingEffect2("CABBY: What is your name?\n");
+            String userName = Console.ReadLine();
+            TypingEffect2("\nCABBY: What can I assist you with " + userName + "?\n");
 
+            while (true)
+            {
+                Console.Write(userName + ": ");
+                string input = Reader();
+                string response = Responsefeature(input);
+
+                TypingEffect2("\n" + "CABBY: " + response + "\n");
+            }
+        }
+
+        public string Responsefeature(string userInput)
+        {
+            userInput = userInput.ToLower();
+
+
+            foreach (var Word in variousResponses.Keys)
+            {
+                if (userInput.Contains(Word))
+                {
+                    return variousResponses[Word];
+                }
+            }
+            return "Sorry, I didn't quite get that, could you rephrase?";
+        }
     }
 }
-
-
